4b66f9a0f0b1dafe10733f196eeffe183c823528
Masterside <masterside@hotmail.de>
2018-05-18 11:13:56 +0200

    Add project files.

---
+++ Client/Connector.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http.Connections;
+using Microsoft.AspNetCore.SignalR.Client;
+using System;
+
+namespace Client
+{
+    public class Connector
+    {
+        public Connector()
+        {
+
+        }
+
+        public async void ConnectToServer(string serveruri)
+        {
+            var connection = new HubConnectionBuilder()
+                                .WithUrl(serveruri, options =>
+                                {
+                                    options.Transports = HttpTransportType.WebSockets;
+                                })
+                                .Build();
+
+            Console.WriteLine($"Connecting to {serveruri}");
+            await connection.StartAsync();
+        }
+    }
+}
---
+++ UI/Program.cs
@@ -0,0 +1,19 @@
+﻿using Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Connector connector = new Connector();
+            connector.ConnectToServer("http://localhost:5000/chat");
+            Console.Read();
+        }
+    }
+}
